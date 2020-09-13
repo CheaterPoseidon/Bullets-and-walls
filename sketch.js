1e9a564c3c25175e91d08108173b57472567bb1a
CheaterPoseidon <70036000+CheaterPoseidon@users.noreply.github.com>
2020-09-13 18:14:17 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var bullet,wall;
+var bulletSpeed,bulletWeight,bulletThickness;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  bulletThickness=random(22,83);
+  bulletSpeed=random(223,321)
+  bulletWeight=random(30,52);  
+  bullet=createSprite(50, 250, 50, 50);
+  bullet.shapeColor
+  bullet.velocityX=bulletSpeed
+  wall=createSprite(1200,200,bulletThickness,200)
+  wall.shapeColor=color(80,80,80)
+
+}
+
+function draw() {
+  background(255,255,255);  
+  if (isTouching(wall,bullet)) {
+    bulletSpeed=0
+    bullet.velocityX=0;
+  }
+  drawSprites();
+}
